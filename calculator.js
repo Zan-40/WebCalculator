aa103dddf425a8b23bc4684f98de12e0900883e8
Alex <alexrodriguezmazariegos@gmail.com>
2023-09-12 13:22:45 +0200

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,6 @@
+function Sumar(){
+    var num1 = document.getElementById('n1').value;
+    var num2 = document.getElementById('n2').value;
+    var suma = parseFloat(num1) + parseFloat(num2);
+    window.alert(suma);
+}
