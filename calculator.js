6c2b71e60ff3a94b3f6c6c36869e07533062c388
Alex <alexrodriguezmazariegos@gmail.com>
2023-09-14 11:33:32 +0200

    Finish

--- calculator.js
+++ calculator.js
@@ -2,5 +2,5 @@ function Sumar(){
     var num1 = document.getElementById('n1').value;
     var num2 = document.getElementById('n2').value;
     var suma = parseFloat(num1) + parseFloat(num2);
-    window.alert(suma);
+    window.alert(num1 + " + " + num2  + " = " + suma);
 }
