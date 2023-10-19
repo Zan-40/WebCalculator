4da1acd1083d04b58156f706823ac0c34d224794
Alex Rodriguez Mazariegos <alexrodriguezmazariegos@gmail.com>
2023-10-19 09:44:49 +0200

    Addition, Difference, Multiplication and Division

--- calculator.js
+++ calculator.js
@@ -4,3 +4,21 @@ function Sumar(){
     var suma = parseFloat(num1) + parseFloat(num2);
     window.alert(num1 + " + " + num2  + " = " + suma);
 }
+function Restar(){
+    var num1 = document.getElementById('n1').value;
+    var num2 = document.getElementById('n2').value;
+    var suma = parseFloat(num1) - parseFloat(num2);
+    window.alert(num1 + " - " + num2  + " = " + suma);
+}
+function Multiplicar(){
+    var num1 = document.getElementById('n1').value;
+    var num2 = document.getElementById('n2').value;
+    var suma = parseFloat(num1) * parseFloat(num2);
+    window.alert(num1 + " x " + num2  + " = " + suma);
+}
+function Dividir(){
+    var num1 = document.getElementById('n1').value;
+    var num2 = document.getElementById('n2').value;
+    var suma = parseFloat(num1) / parseFloat(num2);
+    window.alert(num1 + " / " + num2  + " = " + suma);
+}
